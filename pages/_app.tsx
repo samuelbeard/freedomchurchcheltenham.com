08c0dfcd75aef961dc5642ac9428e7e126cb98e1
Samuel Beard <samjb2@gmail.com>
2022-07-20 19:39:46 +0000

    tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Script
+                strategy="lazyOnload"
+                async
+                defer
+                data-website-id="c46eba03-facf-403e-9242-25e1ed5b1f80"
+                src="https://umami-production-1d5f.up.railway.app/umami.js"
+            ></Script>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
